bbaf91f3d773b6737ceb303af91f5e18b1085b1e
mayur-ios-dev <mayur.deshmukh@voiapp.io>
2022-10-10 15:14:20 +0200

    Initial Commit

---
+++ Husqvarna-Assignment/Husqvarna_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Husqvarna_AssignmentApp.swift
+//  Husqvarna-Assignment
+//
+//  Created by Mayur Deshmukh on 2022-10-10.
+//
+
+import SwiftUI
+
+@main
+struct Husqvarna_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
